ea956ed28ed8e49fe09639ca9b222edd44e71afb
Gustavo <avgustavo@alu.ufc.br>
2022-08-29 15:07:20 -0300

    novos métodos http permitidos sem autenticação

--- src/main/java/br/ufc/mandacaru5/config/security/WebSecurityConfig.java
+++ src/main/java/br/ufc/mandacaru5/config/security/WebSecurityConfig.java
@@ -2,19 +2,21 @@ package br.ufc.mandacaru5.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
-//@Component
-//@EnableWebSecurity	
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
-@Configuration
+@Component
+@EnableWebSecurity	
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Autowired
@@ -35,7 +37,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	public void configure(WebSecurity http) throws Exception {
 		http
 		.ignoring()
-		.antMatchers(HttpMethod.POST, "/api/user");
+		.antMatchers(HttpMethod.POST, "/api/user")
+		.antMatchers(HttpMethod.GET,"/api/user/**/properties")
+		.antMatchers(HttpMethod.GET, "/api/properties", "/api/properties/**" );
 	}
 
 	@Override
