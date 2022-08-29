5cd58a78498f7135b0f5a386f2b80e2e812afb26
Gustavo <avgustavo@alu.ufc.br>
2022-08-29 14:33:56 -0300

    Dispensada a autenticação em alguns métodos

--- src/main/java/br/ufc/mandacaru5/config/security/WebSecurityConfig.java
+++ src/main/java/br/ufc/mandacaru5/config/security/WebSecurityConfig.java
@@ -2,28 +2,40 @@ package br.ufc.mandacaru5.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
 
-@Component
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//@Component
+//@EnableWebSecurity	
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Autowired
 	UserDetailsServiceImpl userDetailsServiceImpl;
 
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http
+//		.httpBasic()
+//		.and()
+//		.authorizeHttpRequests()
+//		.antMatchers(HttpMethod.GET,"/api/user","/api/user/**").permitAll()
+//		.anyRequest().authenticated()
+//		.and()
+//		.csrf().disable();
+//	}
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("api/user").permitAll().and().httpBasic();
+	public void configure(WebSecurity http) throws Exception {
+		http
+		.ignoring()
+		.antMatchers(HttpMethod.POST, "/api/user");
 	}
 
 	@Override
